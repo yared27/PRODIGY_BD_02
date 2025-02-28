058896e1c58c06d7fef8a25774fd4015d4623aeb
Yared <alemayehuyared9@gmail.com>
2025-02-28 09:16:46 -0800

    database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm' ;
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { config } from 'process';
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'crud_app', // Change this to your MySQL username
+      password: 'Yared(2723)', // Change this to your MySQL password
+      database: 'crud_app', // Change this to your database name
+      autoLoadEntities: true,
+      synchronize: true, // S
+        
+
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
